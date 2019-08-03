b417d3a2a8ffa89c5e286761a85af92638223d00
unknown <Emanuel.Tebbe@gmail.com>
2019-08-03 22:18:42 +0200

    initial

---
+++ src/main/java/de/etcomp/buchliste/BuchListeApplication.java
@@ -0,0 +1,13 @@
+package de.etcomp.buchliste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuchListeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuchListeApplication.class, args);
+	}
+
+}
